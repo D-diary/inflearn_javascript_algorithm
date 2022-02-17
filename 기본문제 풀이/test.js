0859e70628cddce08d05dea4d17939053e003248
D-diary <dlrlsud@naver.com>
2022-02-17 22:04:11 +0900

    2022-02-17

---
+++ 기본문제 풀이/test.js
@@ -0,0 +1,20 @@
+function solution(a, b, c) {
+  let answer = "yes"
+  let total = a + b + c;
+  let max = 0;
+
+  if ( a > b ) max = a;
+  else max = b;
+
+  if ( c > max ) max = c;
+
+  if ( total - max <= max ) {
+    return answer = "no"
+  }
+  return answer;
+}
+
+console.log(solution(6, 7, 11));
+console.log(solution(13, 33, 17));
+
+
