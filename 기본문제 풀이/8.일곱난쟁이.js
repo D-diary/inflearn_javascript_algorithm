7f683cf6cd91d2ceb26adcb4fb08ccb768065f37
D-diary <dlrlsud@naver.com>
2022-02-19 01:11:16 +0900

    2022-02-19

--- 기본문제 풀이/8.일곱난쟁이.js
+++ 기본문제 풀이/8.일곱난쟁이.js
@@ -16,42 +16,80 @@
 // 출력예제 1
 // 20 7 23 19 10 8 13
 
+// 첫번쨰 풀이
+
+// 가짜 난쟁이 값을 찾아 a와 b에 넣고 그 값을 제외한 값을 배열에 push
+
 // function solution(arr) {
 //   const answer = [];
 //   let num = 0;
+//   let a = 0, b = 0;
 
 //   for ( let i = 0; i < arr.length; i++) {
 //     num += arr[i]
+//   }
 
+//   for ( let i = 0; i < arr.length; i++) {
 //     for ( let j = i + 1; j < arr.length; j++) {
 //       if ( num - (arr[i] + arr[j]) === 100 ) {
-//         console.log(arr[i], arr[j])
-//         console.log(num - (arr[i] + arr[j]))
+//         a = arr[i];
+//         b = arr[j];
 //       }
 //     }
 //   }
-//   console.log(num)
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if ( arr[i] !== a && arr[i] !== b) {
+//       answer.push(arr[i]);
+//     }
+//   }
+//   console.log(a,b)
 //   return answer;
 
 // }
 
-// console.log(solution([20, 7, 23, 19, 10, 15, 25, 8, 13]))
+// console.log(solution([20, 7, 23, 19, 10, 15, 25, 8, 13]));
+
+// 두번째 풀이
+
+// 답안지의 splice를 활용
+
+// function solution(arr) {
+//   let num = 0;
+//   let answer = arr;
+
+//   for ( let i = 0; i < arr.length; i++) {
+//     num += arr[i]
+//   }
+
+//   for ( let i = 0; i < arr.length; i++) {
+//     for ( let j = i + 1; j < arr.length; j++ ) {
+//       if ( num - ( arr[i] + arr[j] ) === 100 ) {
+//         arr.splice(j, 1);
+//         arr.splice(i, 1);
+//       }
+//     }
+//   }
+//   return answer;
+// }
+
+// console.log(solution([20, 7, 23, 19, 10, 15, 25, 8, 13]));
 
 // 영상 답안
 
-function solution(arr) {
-  let answer = arr;
-  let sum = arr.reduce((a, b) => a+b, 0);
-  for ( let i = 0; i < arr[arr.length -1]; i++ ) {
-    for (let j = i + 1; j < arr.length; j++ ) {
-      if ( sum - ( arr[i] + arr[j] ) === 100 ) {
-        arr.splice(j, 1);
-        arr.splice(i, 1);
-      }
-    }
-  }
+// function solution(arr) {
+//   let answer = arr;
+//   let sum = arr.reduce((a, b) => a+b, 0);
+//   for ( let i = 0; i < arr[arr.length -1]; i++ ) {
+//     for (let j = i + 1; j < arr.length; j++ ) {
+//       if ( sum - ( arr[i] + arr[j] ) === 100 ) {
+//         arr.splice(j, 1);
+//         arr.splice(i, 1);
+//       }
+//     }
+//   }
 
-  return answer;
-}
-let arr = [20, 7, 23, 19, 10, 15, 25, 8, 13];
-console.log(solution(arr));
+//   return answer;
+// }
+// let arr = [20, 7, 23, 19, 10, 15, 25, 8, 13];
+// console.log(solution(arr));
