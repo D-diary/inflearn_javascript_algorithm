ce6ec4f3aa6fb078b1172a2bd9ee82e788bba618
D-diary <dlrlsud@naver.com>
2022-02-28 10:44:11 +0900

    3

--- 1, 2차원 배열 탐색/3. 가위바위보.js
+++ 1, 2차원 배열 탐색/3. 가위바위보.js
@@ -31,34 +31,53 @@
 // B
 // D
 
-function solution(a, b) {
-  let result = '';
+// function solution(a, b) {
+//   let result = '';
   
-  for ( let i = 0; i < a.length; i++ ) {
-    if ( a[i] === b[i] ) result += 'D';
-    if ( a[i] === 1 ) {
-      if ( b[i] === 2 ) {
-        result += 'B';
-      } else if ( b[i] === 3 ){
-        result += 'A';
-      }
-    }
-    if ( a[i] === 2 ) {
-      if ( b[i] === 3 ) {
-        result += 'B';
-      } else if ( b[i] === 1 ) {
-        result += 'A';
-      }
-    }
-    if ( a[i] === 3 ) {
-      if ( b[i] === 1 ) {
-        result += 'B';
-      } else if ( b[i] === 2 ) {
-        result += 'A';
-      }
-    }
+//   for ( let i = 0; i < a.length; i++ ) {
+//     if ( a[i] === b[i] ) result += 'D';
+//     if ( a[i] === 1 ) {
+//       if ( b[i] === 2 ) {
+//         result += 'B';
+//       } else if ( b[i] === 3 ){
+//         result += 'A';
+//       }
+//     }
+//     if ( a[i] === 2 ) {
+//       if ( b[i] === 3 ) {
+//         result += 'B';
+//       } else if ( b[i] === 1 ) {
+//         result += 'A';
+//       }
+//     }
+//     if ( a[i] === 3 ) {
+//       if ( b[i] === 1 ) {
+//         result += 'B';
+//       } else if ( b[i] === 2 ) {
+//         result += 'A';
+//       }
+//     }
+//   }
+//   return result;
+// }
+
+// console.log(solution([2, 3, 3, 1, 3],[1, 1, 2, 2, 3]))
+
+// 답안
+
+function solution(a, b){         
+  let answer="";
+  for(let i=0; i<a.length; i++){
+      if(a[i]===b[i]) answer+="D ";
+      else if(a[i]===1 && b[i]===3) answer+="A ";
+      else if(a[i]===2 && b[i]===1) answer+="A ";
+      else if(a[i]===3 && b[i]===2) answer+="A ";
+      else answer+="B ";
   }
-  return result;
+  
+  return answer;
 }
 
-console.log(solution([2, 3, 3, 1, 3],[1, 1, 2, 2, 3]))
+let a=[2, 3, 3, 1, 3];
+let b=[1, 1, 2, 2, 3];
+console.log(solution(a, b));
