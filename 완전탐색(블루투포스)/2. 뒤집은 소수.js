f8a99dd7430d3f22f84f2aa1a672c2aeb2097ab5
D-diary <dlrlsud@naver.com>
2022-03-15 16:08:28 +0900

    2022-03-15

--- 완전탐색(블루투포스)/2. 뒤집은 소수.js
+++ 완전탐색(블루투포스)/2. 뒤집은 소수.js
@@ -18,3 +18,41 @@ N개의 자연수가 입력되면 각 자연수를 뒤집은 후 그 뒤집은 �
 23 2 73 2 3
 
 */
+
+// function solution(n, arr) {
+
+// }
+
+// console.log(solution(9, [32, 55, 62, 20, 250, 370, 200, 30, 100]));
+
+// 영상 답안
+
+function isPrime(num) {
+  if ( num === 1 ) return false;
+  for ( let i = 2; i <= parseInt(num/2); i++ ) {
+    // i <= parseInt(Math.sqrt(num));
+    if ( num & i === 0 ) return false;
+
+  }
+  return true;
+}
+
+function solution(arr) {
+  let answer = [];
+
+  for ( let x of arr ) {
+    let res = 0;
+    while ( x ) {
+      let t = x % 10;
+      res = res * 10 + t;
+      x = parseInt(x/10);
+    }
+    if ( isPrime(res) ) answer.push(res)
+  }
+  
+  return answer;
+}
+
+let arr = [32, 55, 62, 20, 250, 370, 200, 30, 100];
+console.log(solution(arr));
+
